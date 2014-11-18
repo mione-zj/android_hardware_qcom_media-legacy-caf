47bf67d757a1ae5f405bd6512e5ec0334253daa6
Ethan Chen <intervigil@gmail.com>
2014-11-18 14:13:25 -0800

    media: Use direct project pathmap
    Change-Id: Id402b96e531b4209508103b532732d2614ae9c64
    
    Conflicts:
    	Android.mk
    	dashplayer/Android.mk
    	libc2dcolorconvert/Android.mk
    	mm-video/vidc/vdec/Android.mk
    	mm-video/vidc/venc/Android.mk

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(TARGET_QCOM_MEDIA_VARIANT),legacy-caf)
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8974 msm8960 msm8660 msm7627a msm7630_surf,$(TARGET_BOARD_PLATFORM)),)
